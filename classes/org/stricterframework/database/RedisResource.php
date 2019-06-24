a9280f33db9f7a8cb5901e48a0d64023ef707359
Marcos Rodrigues Gonzalez <mrgonzalez1275@gmail.com>
2019-06-24 20:37:25 -0300

    Ticket #575: Initial Redis support

--- classes/org/stricterframework/database/RedisResource.php
+++ classes/org/stricterframework/database/RedisResource.php
@@ -1,6 +1,6 @@
 <?php
 
-class RedisResource implements Resource, DatabaseInterface
+class RedisResource extends Redis implements Resource
 {
 	private $conn;
 	private $debug = false;
@@ -13,105 +13,19 @@ class RedisResource implements Resource, DatabaseInterface
 	private $dbport;
 	private $redis;
 
-	function __construct(&$config) {
+	function __construct(&$config, $c) {
+
+		parent::__construct();
+
 		$this->dbhost = $config['host'];
 		$this->dbuser = $config['user'];
 		$this->dbpass = $config['password'];
 		$this->dbname = $config['name'];
 		$this->dbport = $config['port'];
 
-		$this->redis = new Redis();
-
 		if(!$this->dbport)
 			$this->dbport=6379;
-
-		$this->redis->connect($this->dbhost, $this->dbport);
-	}
-
-	function connect() {
-		
-	}
-
-	function execute($sql, $params) {
-
-	}
-	
-	function query($sql) {
-		$this->redis->lpush('mylist', $sql);	
-
-	}
-
-	function numrows(&$resource) {
-
-	}
-	
-	function fetch(&$query, $sql_assoc=Database::STRICTER_DB_SQL_ASSOC)	{
-		$v = $this->redis->get('mykey');	
-	}
-
-	function fetchAll(&$query, $sql_assoc=Database::STRICTER_DB_SQL_ASSOC)	{
-
-	}
-
-	function free(&$query) {
-		
-	}
-
-	function lastInsertId($entity) {
-		
-	}
-
-	function disconnect() {
-		
-	}
-
-	function escapeString($string_val) {
-	}
-
-	function error() {
-		
-	}
-
-	function transaction() {
-		
-	}
-
-	function commit() {
-		
-	}
-	
-	function rollback() {
-		
-	}
-
-	function formatField($field) {
-	
-	}
-
-	function paginate(&$query, $limit, $offset) {
-	}
-
-	function setDebug($dbg) {
-		$this->debug=$dbg;
-	}
-
-	function setDbPort($port) {
-		$this->port=$port;
-	}
-
-	function getDbCase() {
-		return $this->dbcase;
-	}
-
-	function getDbType() {
-		return $this->dbtype;
-	}
-	function getSqlStatement() {
-		return $this->sqlStatement;
-	}
-
-	function isConnected() {
-		return $this->connected;
+		$this->connect($this->dbhost,$this->dbport);
 	}
 }
 
