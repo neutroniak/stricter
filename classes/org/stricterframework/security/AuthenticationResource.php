5b314b6b6e4397c576d4c43e206eced770cd6f54
Marcos Rodrigues Gonzalez <mrgonzalez1275@gmail.com>
2019-06-24 20:50:39 -0300

    Ticket #583 (cont): Abstract auth resource
      Create session cookie only on login() successful

--- classes/org/stricterframework/security/AuthenticationResource.php
+++ classes/org/stricterframework/security/AuthenticationResource.php
@@ -12,42 +12,60 @@ class AuthenticationResource implements Resource
 
 		$this->config=$config;
 
-		ini_set('session.hash_function','sha512');
-		ini_set('session.use_strict_mode','On');
-		ini_set('session.sid_length','256');
-
-		if($config['session']['lifetime'])
-			$lifetime=$config['session']['lifetime'];
-
-		session_set_cookie_params($lifetime, "/", null, true, true);
-		session_name("STRICTER");
-		session_start();
+		include_once('security.php');
 
 		$this->stricter=&Stricter::getInstance();
 
+		$path=$this->stricter->getPath();
+
+		if($acl[$path]===false || $path==$config['login-url']) { // open
+			return;
+		}  else {
+			$this->sessionStart();
+			if(!$_SESSION['stricter']) {
+				$this->stricter->redirect('/login');
+			}
+		}
+	}
+
+	public function login($user, $password)	{
 		switch($this->config['method']) {
 			case 'ldap':
 				require_once('org/stricterframework/security/LdapAuthentication.php');
-				$this->auth = new LdapAuthentication($config); 
+				$this->auth = new LdapAuthentication($this->config); 
 				break;
 
 			case 'database':
 
 				break;
 		}
-	}
-
-	public function login($user, $password)	{
-		if($arr=$this->auth->login($user,$password)){
+		
+		if( $arr=$this->auth->login($user,$password) ) {
 			$this->createSession($arr);
 			return true;
 		}
+		else
+		{
+			return false;
+		}
+	}
+
+	private function sessionStart() {
+		ini_set('session.hash_function','sha512');
+		ini_set('session.use_strict_mode','On');
+		ini_set('session.sid_length','256');
+		if($this->config['session']['lifetime'])
+			$lifetime=$this->config['session']['lifetime'];
+		session_set_cookie_params($lifetime, "/", null, true, true);
+		session_name("STRICTER");
+		session_start();	
 	}
 
 	private function createSession($arr) {
+		$this->sessionStart();
 		$_SESSION['stricter']=$arr;
 	}
-	
+
 	public function getError(){
 		return $this->error;
 	}
