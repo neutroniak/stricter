a6b6799c83fecc22c0d78bb5be7939ba17a52bee
Marcos Rodrigues Gonzalez <mrgonzalez1275@gmail.com>
2023-09-13 01:04:20 -0300

    security: ldap check if there is group entry on directory
    When there is not, php81 is halting on ldap_ functions

--- classes/org/stricterframework/security/LdapAuthentication.php
+++ classes/org/stricterframework/security/LdapAuthentication.php
@@ -15,7 +15,7 @@ class LdapAuthentication
 	public function login($user, $password)	{
 		$ldapconn = ldap_connect($this->config['ldap']['host']) or die("Could not connect to LDAP server.");
 		$proto = ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
-		$ldapbind = ldap_bind($ldapconn, 'uid='.$user.','.$this->config['ldap']['users'], $password);
+		$ldapbind = ldap_bind($ldapconn, 'uid='.$user.','.$this->config['ldap']['users'], $password) or die ("Error trying to bind");
 
 		if(!$ldapbind) {
 			$this->error=LANG_LDAP_ERROR;
@@ -23,7 +23,7 @@ class LdapAuthentication
 			return false;
 		}
 
-		$ldapsearch = ldap_search($ldapconn, $this->config['ldap']['users'], "(uid=$user)" );
+		$ldapsearch = ldap_search($ldapconn, $this->config['ldap']['users'], "(uid=$user)" ) or die ("Error in search ");
 		$ldapentries = ldap_get_entries($ldapconn, $ldapsearch);
 
 		if( $ldapentries['count']=='1' ) {
@@ -32,14 +32,20 @@ class LdapAuthentication
 				"sn" => $ldapentries[0]["sn"][0],
 				"uid" => $ldapentries[0]["uid"][0]
 			);
-			$ldapgroups = ldap_search($ldapconn, $this->config['ldap']['groups'], "(memberUid=$user)");
-			$groupentries = ldap_get_entries($ldapconn, $ldapgroups);
-			$agroups=array();
-			foreach($groupentries as $k=>$v) {
-				if($v['cn'][0]!="")
-					array_push($agroups, $v['cn'][0]);
-			}
-			$arr['groups']=$agroups;
+			if($this->config['ldap']['groups']) {
+				try {
+					$ldapgroups = ldap_search($ldapconn, $this->config['ldap']['groups'], "(memberUid=$user)");
+					$groupentries = ldap_get_entries($ldapconn, $ldapgroups);
+					$agroups=array();
+					foreach($groupentries as $k=>$v) {
+						if($v['cn'][0]!="")
+							array_push($agroups, $v['cn'][0]);
+					}
+				} catch(Exception $ldapex) {
+					$this->stricter->log($ldapex);
+				}
+				$arr['groups']=$agroups;
+			}	
 			return $arr;
 		} else {
 			$this->stricter->log( LANG_USER_PASS_NOT_MATCHING );
